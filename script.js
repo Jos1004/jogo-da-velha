593a9e0ebd30fbe695d13a4b8b434ae24d474c83
josiane <josiane0004@gmail.com>
2026-07-26 15:01:06 -0300

    inicializando projeto de criação do jogo da velha

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let n = 1; n < 10; n++){
+    let idn = document.getElementById(`i${n}`)
+    idn.style.backgroundColor = "rgb(31, 54, 65)"
+    console.log(idn.style.backgroundColor)
+}
+
+
+function playerOne(){
+    for(let n = 1; n < 10; n++){
+      let idn = document.getElementById(`i${n}`)
+      idn.onclick = function cor(){
+           if(idn.style.backgroundColor == "rgb(31, 54, 65)"){
+              idn.style.backgroundColor = 'purple'
+            }
+        }
+    }
+}
+
+playerOne()
