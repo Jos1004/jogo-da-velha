b7af88e8f52a3ec446a1271e3efae0248222d79f
josiane <josiane0004@gmail.com>
2026-07-26 15:21:18 -0300

    criando o botão reset

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ for(let n = 1; n < 10; n++){
     console.log(idn.style.backgroundColor)
 }
 
+function reset(){
+    for(let n = 1; n < 10; n++){
+       let idn = document.getElementById(`i${n}`)
+       idn.style.backgroundColor = "rgb(31, 54, 65)"
+    }
+}
 
 function playerOne(){
     for(let n = 1; n < 10; n++){
