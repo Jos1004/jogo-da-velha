66f8757ae66e8bb097380c14acd0cf99619578c3
josiane <josiane0004@gmail.com>
2026-08-10 05:29:41 -0300

    atualizando arquivos

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
-for(let n = 1; n < 10; n++){
-    let idn = document.getElementById(`i${n}`)
-    idn.style.backgroundColor = "rgb(31, 54, 65)"
-    console.log(idn.style.backgroundColor)
-}
-
 function reset(){
     for(let n = 1; n < 10; n++){
        let idn = document.getElementById(`i${n}`)
        idn.style.backgroundColor = "rgb(31, 54, 65)"
     }
 }
+reset()
 
-function game(){
-    function play(){
-      for(let n = 1; n < 9; n++){
-           let idn = document.getElementById(`i${n}`)
+let jogador = 1
 
-           for(let i = 1; i < 9; n++){
-              if(i % 2 === 0){
-                    idn.onclick = function cor(){
-                     if(idn.style.backgroundColor == "rgb(31, 54, 65)"){
-                         idn.style.backgroundColor = 'purple'
-                        }
-                    }
-                }else{
-                     idn.onclick = function cor(){
-                     if(idn.style.backgroundColor == "rgb(31, 54, 65)"){
-                         idn.style.backgroundColor = 'teal'
-                        }
-                    }
-                }
+for(let n = 1; n < 10; n++){
+    let idn = document.getElementById(`i${n}`)
+       idn.onclick = function cor(){
+           if(idn.style.backgroundColor == "rgb(31, 54, 65)"){
+             
+            if(jogador % 2 == 0){
+              idn.style.backgroundColor = 'purple'
+            }else{
+              idn.style.backgroundColor = 'teal'  
             }
-        }
-    }
+               jogador ++
 
-    play()
-}
+            let jogo = [
+                ["","",""],
+                ["","",""],
+                ["","",""]
+            ]
 
-game()
+            let linha = Math.floor((n - 1) / 3)
+            let coluna = (n - 1 % 3)
+
+           jogo[linha][coluna] = "X"
+
+           console.log(jogo)
+
+            if(
+                jogo[0][0] == "X" &&
+                jogo[0][1] == "X" &&
+                jogo[0][2] == "X"
+                ||
+                jogo[1][0] == "X" &&
+                jogo[1][1] == "X" &&
+                jogo[1][2] == "X"
+                ||
+                jogo[2][0] == "X" &&
+                jogo[2][1] == "X" &&
+                jogo[2][2] == "X"
+            ){
+                alert('You Win')
+            }
+       }       
+    } 
+}     
+
+
+
+
+/* ||
+jogo[0][0] == i1 &&
+jogo[1][0] == i4 &&
+jogo[2][0] == i7
+||
+jogo[0][1] == 'X' &&
+jogo[1][1] == 'X' &&
+jogo[2][1] == 'X'
+||
+jogo[0][2] == 'X' &&
+jogo[1][2] == 'X' &&
+jogo[2][2] == 'X'
+||
+jogo[0][2] == 'X' &&
+jogo[1][1] == 'X' &&
+jogo[2][0] == 'X' */
