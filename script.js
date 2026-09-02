826d0b4a80b2602754da7dd075c5578a0aed3e1a
josiane <josiane0004@gmail.com>
2026-09-02 07:12:15 -0300

    testando conexão nova

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let jogo = [
   ['', '', ''],
 ];
 
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('https://jogo-da-velha-hvm7.onrender.com');
 
 // ======================================
 // RESET LOCAL
